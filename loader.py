417aaf5c060a26ba8cc57f88a04e3026d5ce5e58
AurelieP <aurelie.peng@gmail.com>
2022-03-12 01:34:55 +0900

    custom loader

---
+++ loader.py
@@ -0,0 +1,37 @@
+import torch
+import torchvision
+from PIL import Image
+import os
+
+class Loader(torch.utils.data.Dataset):
+    def __init__(self, data_dir, state):
+        self.dir = data_dir
+        self.fileList = [f[:-4] for f in os.listdir(self.dir) if os.path.isfile(os.path.join(self.dir, f)) and ".jpg" in f]
+        self.indexes = range(len(self.fileList))
+        self.dic = {k: self.fileList[k] for k in self.indexes}
+        self.state = state
+        self.resize = 512
+
+    def __len__(self):
+        return len(self.fileList)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        idx = self.indexes[idx]
+        img_name = os.path.join(self.dir, self.dic[idx] + ".jpg")
+        image = Image.open(img_name).convert('L')
+        image.verify()
+
+        if self.state == "train":
+            mirror = torch.rand((2,))
+            if mirror[0] >= 0.5 :
+                image = torchvision.transforms.functional.hflip(image)
+            if mirror[1] >= 0.5 : 
+                image = torchvision.transforms.functional.vflip(image)
+        image = torchvision.transforms.functional.resize(
+            image, size=[self.resize, self.resize])
+        image = (torchvision.transforms.functional.to_tensor(image))
+
+        return image

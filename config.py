b717fc37b5baa7c1e8cce47189f35aee38f04f18
AurelieP <aurelie.peng@gmail.com>
2022-03-12 01:34:44 +0900

    config file for training parameters

---
+++ config.py
@@ -0,0 +1,15 @@
+TRAIN_DIR = "data/train/normal"
+VAL_DIR = "/data/val/normal"
+TEST_DIR = "data/test"
+
+TRAINING_OUTPUT_DIR = "output/20220312_Cookies"
+INFERENCE_OUTPUT_DIR = "output/temp"
+
+BATCH_SIZE = 8
+LR = 0.001
+EPOCHS = 1000
+VAL_VISUALIZATION = 5
+
+DEVICE = "cuda"
+
+PRETRAINED_WEIGHTS = None
